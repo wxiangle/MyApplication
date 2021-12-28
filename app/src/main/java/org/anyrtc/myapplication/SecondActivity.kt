c9280d101b4daa4e3368cbedd1493c2ceeddfa50
wangxiangle <wangxiangle@52toys.com>
2021-12-28 15:09:10 +0800

    init

---
+++ app/src/main/java/org/anyrtc/myapplication/SecondActivity.kt
@@ -0,0 +1,17 @@
+package org.anyrtc.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by wangxl1 on 12/28/21 14:47
+ * E-Mail Address： wang_x_le@163.com
+ */
+class SecondActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+}
